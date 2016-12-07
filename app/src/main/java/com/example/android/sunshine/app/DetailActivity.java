1f695ce8b2de1ffe0adb4dc558114270ae4fe7f7
safirasuli <safi_rasuli@hotmail.com>
2016-12-07 12:30:38 +0100

    Update DetailActivity.java

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * Dit is een test lol
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
